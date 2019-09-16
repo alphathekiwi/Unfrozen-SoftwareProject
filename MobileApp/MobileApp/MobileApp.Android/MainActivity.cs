6da88ba2e8f9ba128dcb6cf9c38c171f7c43bd8b
Alpha <alphathekiwi@gmail.com>
2019-09-16 13:45:14 +1200

    Set app name and logo

--- MobileApp/MobileApp/MobileApp.Android/MainActivity.cs
+++ MobileApp/MobileApp/MobileApp.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace MobileApp.Droid
 {
-    [Activity(Label = "MobileApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Unfrozen Talks", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
