394f08ec1ad3bbc9f6ab421486e085338b168935
Alpha <alphathekiwi@gmail.com>
2019-10-07 13:45:09 +1300

    Added some game logic stuff

---
+++ Game/Unfrozen Game/Assets/Dialog/levelJson.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System;       //Add this line
+using System.Collections.Generic;
+
+[Serializable]
+public class levelJson
+{
+    public string level;
+    public sceneJson[] scenes;
+    public dialogJson[] dialogs;
+    public string[] responses;
+}
+[Serializable]
+public class sceneJson
+{
+    public string name;
+    public int[] dialog;
+}
+[Serializable]
+public class dialogJson
+{
+    public string line;
+    public int attarction;
+    public int uniqueness;
+    public int scene;
+    public int response;
+}
---
+++ Game/Unfrozen Game/Assets/Scripts/Layout_Position.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum orientation
+{
+    HorizontalLeft,
+    HorizontalRight,
+    VerticalTop,
+    VerticalBot
+}
+[RequireComponent(typeof(RectTransform))]
+public class Layout_Position : MonoBehaviour
+{
+    public orientation orientation;
+    public int padding;
+    void Update()
+    {
+        RectTransform rt = GetComponent<RectTransform>();
+        RectTransform pt = transform.parent.GetComponent<RectTransform>();
+        switch (orientation)
+        {
+            case orientation.HorizontalLeft:
+                GetComponent<RectTransform>().localPosition = new Vector2(rt.rect.width / 2 + padding - pt.rect.width / 2, rt.localPosition.y);
+                break;
+            case orientation.HorizontalRight:
+                GetComponent<RectTransform>().localPosition = new Vector2(pt.rect.width / 2 - rt.rect.width / 2 - padding, rt.localPosition.y);
+                break;
+            case orientation.VerticalTop:
+                GetComponent<RectTransform>().localPosition = new Vector2(rt.localPosition.x, pt.rect.height / 2 - rt.rect.height / 2 - padding);
+                break;
+            case orientation.VerticalBot:
+                GetComponent<RectTransform>().localPosition = new Vector2(rt.localPosition.x, rt.rect.height / 2 + padding - pt.rect.height / 2);
+                break;
+        }
+    }
+}
