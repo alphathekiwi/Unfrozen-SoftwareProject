6be31992ce733897295743d6a830df66d9310c38
yararat <yararat@ya.ru>
2019-10-16 16:52:19 +1300

    Added menu etc

---
+++ Game/Unfrozen Game/Assets/Scripts/Level2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Level2 : MonoBehaviour
+{
+    string path;
+    string jsonString;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        path = Application.streamingAssetsPath + "/level2.json";
+        jsonString = File.ReadAllText(path);
+        
+    }  
+}
---
+++ Game/Unfrozen Game/Assets/Scripts/menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu : MonoBehaviour
+{
+    private Animator menu_anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject MyObject = this.gameObject;
+        menu_anim = gameObject.GetComponent<Animator>(); 
+        MyObject.transform.localScale = new Vector3(0, 0, 0);
+        StartCoroutine(ExecuteAfterTime(9, MyObject));
+    }
+
+    
+    IEnumerator ExecuteAfterTime(float time, GameObject MyObject)
+        { 
+            yield return new WaitForSeconds(time);
+            //MyObject.SetActive(true);
+            //MyObject.transform.localScale = new Vector3(12, 12, 12);
+            menu_anim.Play("menu_animation");
+        
+        
+        }
+}
